5a9e47580cb48e88b3c83172cfc2f8f047a4f20f
Matthew Wessel <nucmatt@gmail.com>
2020-05-14 13:05:06 -0500

    Completed responsive styling. Added Readme file template.

--- dist/js/main.js
+++ dist/js/main.js
@@ -102,7 +102,7 @@ const outputHtml = (weaponList) => {
 				`
                     <div class="weapon" draggable="true">
                         <div class="row">
-                            <p class="col-4 wpn-name"><span class=${famedList.includes(weapon.name) ? `${weapon.name.toLowerCase().replace(/\s/g, "")}` : "standard"}>${
+                            <p class="col-4 wpn-name"><span class=${famedList.includes(weapon.name) ? `${weapon.name.toLowerCase().replace(/\s/g, "")} style=font-style:italic;font-weight:bold`  : "standard"}>${
 															weapon.name
 														}</span><span class="buttons"><button type="button" class="toggle"><i class="fa fa-chevron-right show_stats rotateRight"></i></button></span>
                             </p>
